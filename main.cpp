0ee69d1b780590e60b49d8d7aceb836a8a3b1edc
Glen Sasek <glen.sasek@pcc.edu>
2025-03-05 11:47:53 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//*****************************************************************************
+// Author: 
+// Assignment:
+// Date:
+// Description:
+// Sources: 
+//*****************************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
