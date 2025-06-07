80927b7558db6242dc17ed9550330a1b15980ae5
simon-wong-pcc <simon.wong1@pcc.edu>
2025-06-07 06:12:48 +0000

    assignment 4

---
+++ assignment-4/main.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int main() {
+
+    TaskList taskList;
+    taskList.read("tasks.txt");
+
+    cout << "Welcome!\n";
+    cout << "This program will help you manage your tasks for this Space Station.\n\n";
+
+    do {
+        printMenu();
+    } while (!handleInput(taskList));
+
+    taskList.write("tasks.txt");
+
+    cout << "Tasks written to file! Thank you for using my program!!";
+
+}
+
+void printMenu() {
+    cout << "\nPick an option from below:\n\n";
+    cout << "(a)Add a new task\n";
+    cout << "(b)List tasks by name\n";
+    cout << "(c)List tasks by Type\n";
+    cout << "(d)Search by task name\n";
+    cout << "(e)Remove tasks by index\n";
+    cout << "(f)Print tasks by reverse name\n";
+    cout << "(q)Quit\n" << endl;    
+}
+
+bool handleInput(TaskList& taskList) {
+
+    char userSelection;
+    bool exitSignal = false;
+
+    cin >> userSelection;
+    cin.ignore(100, '\n');
+
+    switch (userSelection) {
+    case 'a': // Add
+        addTask(taskList);
+        break;
+    case 'b': // List
+        taskList.print();
+        break;
+    case 'c': // List by type
+        listTypeTasks(taskList);
+        break;
+    case 'd': // Search
+        searchTask(taskList);
+        break;
+    case 'e': // Remove
+        removeTask(taskList);
+        break;
+    case 'f':
+        taskList.reverse();
+        taskList.print();
+        taskList.reverse();
+        break;
+    case 'q': // Quit
+        exitSignal = true;
+        break;
+    default:
+        cout << "Invalid option!! Please try again!" << endl;
+    }
+
+    return exitSignal;
+}
+
+void addTask(TaskList& taskList){
+    if (taskList.getSize() >= MAX_ARR_SIZE) {
+        cout << "Error: Max size reached!" << endl;
+        return;
+    }
+
+    Task task = getUserTask();
+    taskList.insert(task);
+
+    cout << "\nTask added!\n\n";
+
+    taskList.print();
+}
+
+
+void listTypeTasks(TaskList& taskList){
+    TaskType type = (TaskType) getUserInt("Enter Type number(0-Operations, 1-Maintenance, 2-Inventory, 3-Communications, and 4-Others): ", 0, 4);
+    taskList.printByType(type);
+}
+
+void searchTask(TaskList& taskList){
+    char str[MAX_STRLEN];
+    getUserString(str, "Enter task name: ");
+    taskList.search(str);
+}
+
+void removeTask(TaskList& taskList){
+    if (taskList.getSize() <= 0) {
+        cout << "Error: No tasks in list!" << endl;
+        return;
+    }
+
+    taskList.print();
+    cout << "\n";
+    int idx = getUserInt("Enter index of task to remove: ", 1, taskList.getSize());
+    taskList.remove(idx - 1);
+    cout << "Task Removed!\n";
+    taskList.print();
+}
+
+
+Task getUserTask() {
+
+    Task out;
+
+    char stemp[MAX_STRLEN];
+    int itemp;
+
+    itemp = getUserInt("Enter the day of the task (whole numbers between 1 and 30): ", 1, 30);
+    out.setDay(itemp);
+
+    getUserString(stemp, "Enter the task name (50 characters or less): ");
+    out.setName(stemp);
+
+    getUserString(stemp, "Enter the person’s name (50 characters or less): ");
+    out.setPerson(stemp);
+
+    itemp = getUserInt("Enter the number of hours (whole numbers between 1 and 10): ", 1, 10);
+    out.setDuration(itemp);
+
+    itemp = getUserInt("Enter the task type 0-Operations, 1-Maintenance, 2-Inventory, 3-Communications, and 4-Others): ", 0, 4);
+    out.setType((TaskType) itemp);
+
+    return out;
+}
+
+void getUserString(char* string, const char* prompt) {
+    do {
+        cout << prompt;
+        cin.getline(string, MAX_STRLEN);
+        if (cin.fail()) {
+            cout << "Invalid Input! Must be less than " << MAX_STRLEN - 1 << " characters!" << endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+        }
+    } while (cin.fail());
+}
+
+int getUserInt(const char* prompt, int lower, int upper) {
+    int out;
+
+    do {
+        cout << prompt;
+        cin >> out;
+        cin.ignore(5, '\n');
+
+        if (cin.fail() || out < lower || out > upper) {
+            cout << "Error! Enter a number between " << lower << " and " << upper << "!\n";
+            out = lower - 1;
+            cin.clear();
+        }
+    } while (out < lower || out > upper);
+
+    return out;
+}
---
+++ assignment-4/tasklist.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "task.h"
+
+const int MAX_ARR_SIZE = 30;
+
+class TaskList {
+public:
+
+    TaskList();
+
+    TaskList(const TaskList& other);
+
+    ~TaskList();
+
+    TaskList& operator=(const TaskList& other);
+
+    void insert(Task task);
+    void remove(int index);
+    void reverse();
+
+    Task& at(int index) const;
+
+    void print();
+    void printByType(TaskType type);
+    void search(char* filter);
+
+    void read(const char* filename);
+    void write(const char* filename);
+
+    int getSize () { return size; }
+
+private:
+    struct Node {
+        Task data;
+        Node* next;
+    };
+
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int size = 0;
+};
